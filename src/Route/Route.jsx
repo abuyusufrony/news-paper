255b7320116b87626e5a4e8701b270389a8021b6
abuyusufrony <abuyusufrony@gmail.com>
2025-04-30 18:25:10 +0600

    root-set-up

---
+++ src/Route/Route.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react';
+import { createBrowserRouter } from 'react-router';
+
+const Route = createBrowserRouter([{
+
+    path: '/',
+    element: <h1>This is root</h1>
+}])
+
+
+export default Route;
